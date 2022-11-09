01f3ad77b7fbd7cc8531a3fcc1b5d662d20bd63c
WahyuSapurata <wm337708@gmail.com>
2022-11-09 23:57:52 +0800

    modified header

--- resources/views/components/heros/main.blade.php
+++ resources/views/components/heros/main.blade.php
@@ -1,12 +1,14 @@
-<div>
+<div
+    style="background: linear-gradient(180deg, #f1602b, #fa3c0f); border-bottom-right-radius: 30px; border-bottom-left-radius: 30px;
+">
     <!-- Well begun is half done. - Aristotle -->
-    <nav class="navbar bg-primary">
-        <div class="container">
+    <nav class="navbar navbar-expand-md">
+        <div class="container d-block">
             <div class="navbar-collapse d-flex" id="navbarSupportedContent">
                 <a class="navbar-brand" href="#">
                     <img src="{{ asset('assets/images/logo-header.png') }}" alt="{{ env('APP_NAME') }}" class="img-header">
                 </a>
-                <form autocomplete="off" action="" method="GET" class="w-100">
+                <form autocomplete="off" action="" method="GET" class="w-100 hiden">
                     <div class="position-relative d-flex justify-content-end">
                         <input type="text" name="search" class="form-control custom-search search-control"
                             placeholder="Cari kebutuhan anda">
@@ -15,21 +17,42 @@
                                 class="bi bi-search"></i></button>
                     </div>
                 </form>
-                <ul class="navbar-nav">
+                <ul class="navbar-nav ms-3 gap-3">
                     <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="#">Home</a>
+                        <a class="nav-link" aria-current="page" title="toko" href="#"><i
+                                class="bi bi-shop text-white fs-4"></i></a>
                     </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Features</a>
+                    <li class="nav-item hiden">
+                        <a class="nav-link" aria-current="page" title="keranjang" href="#"><i
+                                class="bi bi-cart4 text-white fs-4"></i></a>
                     </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Pricing</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link disabled">Disabled</a>
+                    <li class="nav-item hiden">
+                        <a class="nav-link" aria-current="page" title="profil" href="#"><i
+                                class="bi bi-person-circle text-white fs-4"></i></a>
                     </li>
                 </ul>
             </div>
+            <div class="sub-navbar">
+                <div class="content-user">
+                    <div class="user">
+                        <i class="bi bi-person-fill"></i>
+                        <h6 class="m-0 text-white fw-bolder">Name</h6>
+                    </div>
+                    <div class="text-user text-white">
+                        <small>saldo anda saat ini</small>
+                        <h5 class="m-0"><b>Rp 0</b></h5>
+                        <a href="" class="btn btn-custom hiden"><i class="bi bi-wallet-fill"></i> isi saldo</a>
+                    </div>
+                </div>
+                <div class="content-saldo">
+                    <i class="bi bi-cash-coin"></i>
+                    <div class="saldo-name text-white">
+                        <h5 class="m-0 fw-bolder">POIN 0</h5>
+                        <small>Kumpulkan poin anda</small>
+                    </div>
+                </div>
+            </div>
         </div>
     </nav>
+
 </div>
